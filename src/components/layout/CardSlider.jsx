8ceaf88805b2d8095e5a51c560c3ca6584a592f2
Katrin <katrin.brueheim@gmx.de>
2024-03-29 23:07:45 +0100

    Fixed bug in card slider.

--- src/components/layout/CardSlider.jsx
+++ src/components/layout/CardSlider.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const CardSlider = ({ cards }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const isMobile = useBreakpointValue({ base: true, md: false });
 
   const prevSlide = () => {
     setCurrentIndex((oldIndex) => (oldIndex === 0 ? cards.length - 1 : oldIndex - 1));
@@ -16,13 +15,13 @@ const CardSlider = ({ cards }) => {
 
   return (
     <Box position="relative" height="100%" width="100%">
-      {isMobile && <IconButton aria-label="Previous Slide" icon={<FaChevronLeft />} position="absolute" left="2" top="50%" transform="translate(0, -50%)" zIndex="2" onClick={prevSlide} />}
+      <IconButton aria-label="Previous Slide" icon={<FaChevronLeft />} position="absolute" left="2" top="50%" transform="translate(0, -50%)" zIndex="2" onClick={prevSlide} />}
       {cards.map((card, index) => (
         <Box key={index} position="absolute" top="0" left="0" right="0" bottom="0" opacity={index === currentIndex ? "1" : "0"} transition="opacity 0.5s">
           {card}
         </Box>
       ))}
-      {isMobile && <IconButton aria-label="Next Slide" icon={<FaChevronRight />} position="absolute" right="2" top="50%" transform="translate(0, -50%)" zIndex="2" onClick={nextSlide} />}
+      <IconButton aria-label="Next Slide" icon={<FaChevronRight />} position="absolute" right="2" top="50%" transform="translate(0, -50%)" zIndex="2" onClick={nextSlide} />}
     </Box>
   );
 };
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,19 +5,38 @@ import WhaleWatchCard from "../components/market/cards/WhaleWatchCard";
 import TopCryptoCard from "../components/market/cards/TopCryptoCard";
 import CryptoTable from "../components/market/CryptoTable";
 import TradingTips from "../components/common/TradingTips";
-import { Grid, Box } from "@chakra-ui/react";
-import CardSlider from "../components/common/CardSlider";
+import { Grid, Box, useBreakpointValue } from "@chakra-ui/react";
+import CardSlider from "../components/layout/CardSlider";
 
 const Index = ({ assets, marketData }) => {
   const { bitcoinData, btcDominance, ethDominance, totalVolume, marketDirection, totalMarketCap } = marketData;
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const cards = [
+    <TopCryptoCard assets={assets} />, 
+    <FearGreedIndexCard bitcoinData={bitcoinData} btcDominance={btcDominance} ethDominance={ethDominance} totalVolume={totalVolume} marketDirection={marketDirection} totalMarketCap={totalMarketCap} />, 
+    <WhaleWatchCard />
+  ];
+
+  const mobileCardSlider = isMobile && (
+    <CardSlider cards={cards} />
+  );
+
+  const desktopCards = !isMobile && (
+    <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap={8} my={2} mx={{ base: 4, md: 8 }} maxWidth="1200px" alignItems="center">
+      { cards }
+    </Grid>
+  );
+
   return (
     <>
       <MarketTeaser assets={assets} mb={4} />
       <Box my={2} mx={{ base: 4, md: 8 }} maxWidth="1200px" height="210px">
-        <CardSlider cards={[<TopCryptoCard assets={assets} />, <FearGreedIndexCard bitcoinData={bitcoinData} btcDominance={btcDominance} ethDominance={ethDominance} totalVolume={totalVolume} marketDirection={marketDirection} totalMarketCap={totalMarketCap} />, <WhaleWatchCard />]} />
+        { mobileCardSlider }
+        { desktopCards }
+        <CryptoTable assets={assets} />
+        <TradingTips />
       </Box>
-      <CryptoTable assets={assets} />
-      <TradingTips />
     </>
   );
 };

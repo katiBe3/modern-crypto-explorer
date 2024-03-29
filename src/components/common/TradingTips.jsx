18f71d7e5dea29df57dcfecdfb9c21ed4c132df0
Katrin <katrin.brueheim@gmx.de>
2024-03-29 15:51:12 +0100

    Tweaked trading tips design.

--- src/components/common/TradingTips.jsx
+++ src/components/common/TradingTips.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, Box, Heading } from "@chakra-ui/react";
+import { Text, Box, Heading, Button } from "@chakra-ui/react";
 import { tradingTips } from  "../../data/TradingTips";
 
 const TradingTips = () => {
@@ -13,15 +13,19 @@ const TradingTips = () => {
   }
 
   return (
-    <Box bg="#5A4FCF" width="full" py={{ base: 10, md: 20 }} px={4} align="center">
+    <Box bg="#5A4FCF" width="full" py={{ base: 10, md: 20 }} px={4} align="center" color="white">
+      <Heading textAlign="center" fontWeight="bold" mb={4} fontSize={ "xl" }>Trade Like a Pro</Heading>
       <Box maxWidth="640px">
-      <Heading textAlign="center" color="white" fontWeight="bold" mb={2} fontSize={{ base: "2xl", md: "3xl" }}>
-        {currentTip.title}
+        <Heading textAlign="center" fontWeight="bold" mb={2} fontSize={{ base: "2xl", md: "3xl" }}>
+          {currentTip.title}
         </Heading>
-      <Text textAlign="center" color="white" mb={4} fontSize={{ base: "md", md: "lg" }}>
-        {currentTip.text}
-      </Text>
+        <Text textAlign="center" color="white" mb={4} fontSize={{ base: "md", md: "lg" }}>
+          {currentTip.text}
+        </Text>
       </Box>
+      <Button bg="white" color="5A4FCF" fontWeight="bold" px="4" textShadow="0 0 8px rgba(255, 255, 255, 0.4)">
+        Show More
+      </Button>
     </Box>
   );
 };

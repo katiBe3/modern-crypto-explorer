89967962f6a00b1f9859b72a3cefece4ca2990de
Katrin <katrin.brueheim@gmx.de>
2024-03-31 16:50:37 +0200

    Added brand color to top bar.

--- src/components/layout/Bar.jsx
+++ src/components/layout/Bar.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 
 const Bar = ({ children }) => {
   return (
-    <Box bg="gray.800" py={2} px={4} overflowX="auto" whiteSpace="nowrap">
+    <Box bg="brand.main" py={2} px={4} overflowX="auto" whiteSpace="nowrap">
         <Box color="white" textAlign="left" fontSize="sm" display="flex" alignItems="center">
         {children}
         </Box>

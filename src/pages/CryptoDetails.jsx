312ce4bc27af8fc230785439c4e7d89b28ed4a82
Katrin <katrin.brueheim@gmx.de>
2024-04-01 21:34:34 +0200

    Changed heading formatting in crypto details page.

--- src/pages/CryptoDetails.jsx
+++ src/pages/CryptoDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 import NewsSection from "../components/news/NewsSection";
 import ExchangeTable from "../components/market/table/ExchangeTable";
 
@@ -13,10 +13,10 @@ const CryptoDetails = ({ assets }) => {
   }
 
   return (
-    <Box mx="auto" maxWidth="1200px">
-      <Text fontSize="2xl" fontWeight="bold">
-      {crypto.name} ({crypto.symbol}) - <Text as="span" color="green.500">{parseFloat(crypto.priceUsd) >= 100 ? `$${parseInt(crypto.priceUsd).toLocaleString()}` : parseFloat(crypto.priceUsd) >= 1 ? `$${parseFloat(crypto.priceUsd).toLocaleString()}` : `$${parseFloat(crypto.priceUsd).toFixed(8)}`}</Text>
-      </Text>
+    <Box px={8} py={4} maxWidth="1200px" mx="auto" textAlign="center">
+      <Heading as="h1" size="xl" mb={4} textAlign="center">
+        {crypto.name} ({crypto.symbol}) - <Text as="span" color="green.500">{parseFloat(crypto.priceUsd) >= 100 ? `$${parseInt(crypto.priceUsd).toLocaleString()}` : parseFloat(crypto.priceUsd) >= 1 ? `$${parseFloat(crypto.priceUsd).toLocaleString()}` : `$${parseFloat(crypto.priceUsd).toFixed(8)}`}</Text>
+      </Heading>
       <ExchangeTable cryptoId={crypto.id} />
       <NewsSection cryptos={crypto.symbol} />
     </Box>

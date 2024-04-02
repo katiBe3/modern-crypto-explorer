7a43a714a90e447c7cbf42c5320d9dd395c725ff
Katrin <katrin.brueheim@gmx.de>
2024-04-02 20:35:08 +0200

    UI improvement: Added skeleton to news card component.

--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Box, Flex, Heading, Text, Skeleton, Image } from "@chakra-ui/react";
 import Card from "../layout/Card";
 
 const NewsCard = ({ newsItem }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 900); // Timeout set to just below 1 second.
+
+    return () => clearTimeout(timer); // Clear the timer on component unmount
+  }, []);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
+  const handleImageError = () => {
+    setLoading(false);
+  };
+
   const truncateText = (text, maxLength) => {
     if (text.length > maxLength) {
       return text.substring(0, maxLength) + "...";
@@ -14,14 +32,32 @@ const NewsCard = ({ newsItem }) => {
     <Card>
       <Box as="a" href={newsItem.url} target="_blank" _hover={{ textDecoration: "none" }}>
         <Flex>
-          <Image src={newsItem.imageurl} alt={newsItem.title} boxSize={{ base: "full", md: "175px" }} objectFit="cover" mr={4} borderRadius="8px" />
+          {loading ? (
+            <Skeleton height="150px" width="150px" borderRadius="8px" mr={4} />
+          ) : (
+            <Image
+              src={newsItem.imageurl}
+              alt={newsItem.title}
+              onLoad={handleImageLoad}
+              onError={handleImageError}
+              borderRadius="8px"
+              mr="12px"
+              objectFit="cover"
+              boxSize="150px"
+              maxWidth="150px"
+            />
+          )}
           <Box align="left">
-            <Heading size="sm" mb={2}>
-              {truncateText(newsItem.title, 100)}
-            </Heading>
-            <Text fontSize="sm" color="gray.500" fontWeight="bold" mb={2}>
-              {newsItem.source_info.name}
-            </Text>
+            <Skeleton isLoaded={!loading}>
+              <Heading size="sm" mb={2}>
+                {truncateText(newsItem.title, 100)}
+              </Heading>
+            </Skeleton>
+            <Skeleton isLoaded={!loading}>
+              <Text fontSize="sm" color="gray.500" fontWeight="bold" mb={2}>
+                {newsItem.source_info.name}
+              </Text>
+            </Skeleton>
           </Box>
         </Flex>
       </Box>
